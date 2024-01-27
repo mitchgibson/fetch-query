2a7241176227f921b21624f08ca32c45a0339ebe
knakmitch <mitch.delachevrotitere@knak.com>
2024-01-27 06:06:21 -0500

    add annotations

--- src/fetch-query.ts
+++ src/fetch-query.ts
@@ -11,14 +11,14 @@ export type FetchQueryError = {
  * FetchQuery is a wrapper around the fetch API that provides a simple interface for making requests.
  *
  * @example const { error, loading, data } = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1').query();
- * 
+ *
  * console.log(error, loading, data);
  * @example const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1');
- * 
+ *
  * fetch.onLoading((loading) => console.log(loading));
  * fetch.onData((data) => console.log(data));
  * fetch.onError((error) => console.log(error));
- * 
+ *
  * fetch.query();
  * @example const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1', { method: 'POST', body: { id: 1 } });
  * const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1');
@@ -26,7 +26,6 @@ export type FetchQueryError = {
  * @example const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1');
  * fetch.setRetries(3);
  * fetch.query({ method: 'POST', body: { id: 1 } });
-
  */
 export class FetchQuery<T> {
   private _data: T | null = null;
@@ -38,32 +37,63 @@ export class FetchQuery<T> {
   private data$: BehaviorSubject<T | null> = new BehaviorSubject<T | null>(this._data);
   private error$: BehaviorSubject<FetchQueryError | null> = new BehaviorSubject<FetchQueryError | null>(this._error);
 
+  /**
+   *
+   * @param url { string | Request}
+   * @param options { RequestInit }
+   */
   constructor(private url: string | Request, private options?: RequestInit) {}
 
+  /**
+   * Get the current value of the response data. Returns null if there is no data.
+   * @returns { T | null }
+   */
   public get data(): T | null {
     return this._data;
   }
 
+  /**
+   * Get the current value of the response error. Returns null if there is no error.
+   */
   public get error(): FetchQueryError | null {
     return this._error;
   }
 
+  /**
+   * Get the current value of the loading state.
+   */
   public get loading(): boolean {
     return this._loading;
   }
 
+  /**
+   * Add a listener for loading state changes.
+   * @param callback
+   */
   public onLoadingChange(callback: (loading: boolean) => void): void {
     this.loading$.subscribe(callback);
   }
 
+  /**
+   * Add a listener for response data changes.
+   * @param callback
+   */
   public onDataChange(callback: (data: T | null) => void): void {
     this.data$.subscribe(callback);
   }
 
+  /**
+   * Add a listener for response error changes.
+   * @param callback
+   */
   public onErrorChange(callback: (error: FetchQueryError | null) => void): void {
     this.error$.subscribe(callback);
   }
 
+  /**
+   * Set the number of times to retry the request if it fails.
+   * @param maxRetryCount
+   */
   public setRetries(maxRetryCount: number): void {
     if (maxRetryCount < 0) {
       throw new Error("maxRetryCount must be greater than or equal to 0");
@@ -71,6 +101,11 @@ export class FetchQuery<T> {
     this._maxRetryCount = maxRetryCount;
   }
 
+  /**
+   * Make a request to the url provided in the constructor.
+   * @param option { RequestInit }
+   * @returns { FetchQuery<T> }
+   */
   public query(options: RequestInit | undefined = this.options): FetchQuery<T> {
     this.updateLoading(true);
     this.updateError(null);
