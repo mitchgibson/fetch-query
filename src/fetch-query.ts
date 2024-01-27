3e37cf4feae5ab0017c08045dd0c7d0ef1c7e21d
knakmitch <mitch.delachevrotitere@knak.com>
2024-01-27 05:49:09 -0500

    initial commit

---
+++ src/fetch-query.ts
@@ -0,0 +1,116 @@
+import { retry, switchMap, take } from "rxjs/operators";
+import { fromFetch } from "rxjs/fetch";
+import { BehaviorSubject, from } from "rxjs";
+
+export type FetchQueryError = {
+  status: number;
+  message: string;
+};
+
+/**
+ * KnakFetch is a wrapper around the fetch API that provides a simple interface for making requests.
+ *
+ * @example const { error, loading, data } = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1').query();
+ * @example const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1');
+ * fetch.onLoading((loading) => console.log(loading));
+ * fetch.onData((data) => console.log(data));
+ * fetch.onError((error) => console.log(error));
+ * fetch.query();
+ * @example const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1', { method: 'POST', body: { id: 1 } });
+ * const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1');
+ * fetch.query();
+ * @example const fetch = new FetchQuery<MyThing>('https://jsonplaceholder.typicode.com/todos/1');
+ * fetch.setRetries(3);
+ * fetch.query({ method: 'POST', body: { id: 1 } });
+
+ */
+export class FetchQuery<T> {
+  private _data: T | null = null;
+  private _error: FetchQueryError | null = null;
+  private _loading: boolean = false;
+  private _maxRetryCount: number = 0;
+
+  private loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this._loading);
+  private data$: BehaviorSubject<T | null> = new BehaviorSubject<T | null>(this._data);
+  private error$: BehaviorSubject<FetchQueryError | null> = new BehaviorSubject<FetchQueryError | null>(this._error);
+
+  constructor(private url: string | Request, private options?: RequestInit) {}
+
+  public get data(): T | null {
+    return this._data;
+  }
+
+  public get error(): FetchQueryError | null {
+    return this._error;
+  }
+
+  public get loading(): boolean {
+    return this._loading;
+  }
+
+  public onLoadingChange(callback: (loading: boolean) => void): void {
+    this.loading$.subscribe(callback);
+  }
+
+  public onDataChange(callback: (data: T | null) => void): void {
+    this.data$.subscribe(callback);
+  }
+
+  public onErrorChange(callback: (error: FetchQueryError | null) => void): void {
+    this.error$.subscribe(callback);
+  }
+
+  public setRetries(maxRetryCount: number): void {
+    if (maxRetryCount < 0) {
+      throw new Error("maxRetryCount must be greater than or equal to 0");
+    }
+    this._maxRetryCount = maxRetryCount;
+  }
+
+  public query(options: RequestInit | undefined = this.options): FetchQuery<T> {
+    this.updateLoading(true);
+    this.updateError(null);
+    fromFetch(this.url, options)
+      .pipe(
+        take(1),
+        switchMap((response) => {
+          if (response.ok) {
+            return from(response.json());
+          }
+          throw response;
+        }),
+        retry(this._maxRetryCount)
+      )
+      .subscribe({
+        next: (response) => {
+          this.updateData(response);
+        },
+        error: (error) => {
+          this.updateError({
+            status: error.status,
+            message: error.statusText,
+          });
+        },
+        complete: () => {
+          this.updateLoading(false);
+        },
+      });
+
+    return this;
+  }
+
+  private updateLoading(loading: boolean): void {
+    this._loading = loading;
+    this.loading$.next(loading);
+  }
+
+  private updateData(data: T | null): void {
+    this._data = data;
+    this.data$.next(data);
+  }
+
+  private updateError(error: FetchQueryError | null): void {
+    this._error = error;
+    this.error$.next(error);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,64 @@
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "/vite.svg";
+import { FetchQuery } from "./fetch-query.ts";
+
+const query = new FetchQuery<any>("https://jsonplaceholder.typicode.com/todos/1");
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="query" type="button">Query</button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+    <div>Loading: ${query.loading}</div>
+    <div>Error: ${JSON.stringify(query.error)}</div>
+    <div>Data: ${JSON.stringify(query.data)}</div>
+  </div>
+`;
+
+query.onDataChange((data) => {
+  console.log("data", data);
+});
+
+query.onErrorChange((error) => {
+  console.log("error", error);
+});
+
+query.onLoadingChange((loading) => {
+  console.log("loading", loading);
+});
+
+let queryData: FetchQuery<any>;
+
+function draw() {
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <div>Loading: ${queryData.loading}</div>
+    <div>Error: ${JSON.stringify(queryData.error)}</div>
+    <div>Data: ${JSON.stringify(queryData.data)}</div>
+  `;
+}
+
+let id: number;
+
+document.querySelector<HTMLButtonElement>("#query")!.addEventListener("click", () => {
+  if (id) clearInterval(id);
+  id = setInterval(() => {
+    draw();
+  }, 3000);
+
+  query.setDebounceTime(1000);
+  for (let i = 0; i < 5; i++) {
+    queryData = query.query();
+  }
+  draw();
+});
